f0239632f63ac4aff9cea3d0be19985114d8c880
3p-vln <vldknovak@gmail.com>
2024-11-26 14:42:19 +0200

    fix scss backgtound main page + start about company with bg

--- pages.config.js
+++ pages.config.js
@@ -5,6 +5,10 @@ const pages = [
     name: 'index',
     path: resolve(__dirname, 'index.html'),
   },
+  {
+    name: 'about-company',
+    path: resolve(__dirname, 'about-company.html'),
+  },
 ];
 
 export default pages;
