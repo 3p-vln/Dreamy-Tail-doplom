26539c0f57d8360973cc085b053a6ae12ab212d5
3p-vln <vldknovak@gmail.com>
2024-11-27 19:23:36 +0200

    + preloader

--- src/ts/about-company.ts
+++ src/ts/about-company.ts
@@ -1,10 +1,9 @@
 import { Preloader } from './components/preloader';
 import { Loader } from './modules/stop-preload';
-import { RegFormValidation } from './registration/reg-form-validate';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  new RegFormValidation('#form-reg');
-  Loader.stop('reg-form__form');
+  Loader.stop('mission-and-vision__right-img');
+  Loader.stop('galery');
 });
 
 new Preloader();
--- src/ts/donate.ts
+++ src/ts/donate.ts
@@ -1,7 +1,12 @@
+import { Preloader } from './components/preloader';
 import { TabValidation } from './donate/pay-validate';
 import { Tab } from './donate/tabs';
+import { Loader } from './modules/stop-preload';
 
 document.addEventListener('DOMContentLoaded', async () => {
   new Tab('.tabs__nav-btn', '.tabs__item');
   new TabValidation();
+  Loader.stop('form__tabs');
 });
+
+new Preloader();
